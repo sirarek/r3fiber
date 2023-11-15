ea6f87fb108a19fa6b547ca0d33ae68e57e5f684
arek <76258557+sirarek@users.noreply.github.com>
2023-11-15 23:30:40 +0000

    Update Lights.js

--- src/components/Lights.js
+++ src/components/Lights.js
@@ -1,19 +1,41 @@
-import { useRef } from "react"
-import { PointLightHelper } from "three"
-import { useHelper } from "@react-three/drei"
-
-
+import { useRef } from "react";
+import { PointLightHelper } from "three";
+import { SphereGeometry, MeshBasicMaterial } from "three";
+import { useHelper, Sphere } from "@react-three/drei";
+const SphereLight = () => {
+  return (
+    <Sphere scale={0.15}>
+      <meshPhysicalMaterial
+        color="white"
+        emissive="white"
+        emissiveIntensity={6}
+      />
+    </Sphere>
+  );
+};
 const Lights = () => {
-    const pointShadowCameraRef = useRef();
+  const pointShadowCameraRef = useRef();
 
-    const light = useRef()
-    useHelper(light,PointLightHelper, 0.5,'black')
-    return  (<pointLight intensity={1} position={[0,2.2,0]} ref={light} castShadow   shadow-mapSize={[1024, 1024]}
-        shadow-bias={-0.00002}
-        shadow-normalBias={0.03}
-        shadow-radius={5}>
-            <perspectiveCamera ref={pointShadowCameraRef} attach="shadow-camera" args={[90, 1, 0.1, 10]} />
-        </pointLight>
-)
-}
-export default Lights
+  const light = useRef();
+  //   useHelper(light, PointLightHelper, 0.5, "black");
+  return (
+    <pointLight
+      intensity={1}
+      position={[0, 2.2, 0]}
+      ref={light}
+      castShadow
+      shadow-mapSize={[1024, 1024]}
+      shadow-bias={-0.00002}
+      shadow-normalBias={0.03}
+      shadow-radius={5}
+    >
+      <SphereLight />
+      <perspectiveCamera
+        ref={pointShadowCameraRef}
+        attach="shadow-camera"
+        args={[90, 1, 0.1, 10]}
+      />
+    </pointLight>
+  );
+};
+export default Lights;
